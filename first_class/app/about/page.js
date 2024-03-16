e7b1f101498dcc95c3b0da2aa993e0b26370e352
hpnavadiya <navadiyaharsh8@gmail.com>
2024-03-16 17:23:12 -0400

    Done with introduction of NextJS

---
+++ first_class/app/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>This is About page.</div>
+  )
+}
+
+export default about
---
+++ first_class/app/page.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div>
+        It Is Home Page
+      </div>
+      <h1 className="teaxt-xl">
+        Problem solved by NextJS
+      </h1>
+      <ul>
+        <li>File base route - We can create folder inside app folder based on route name like about, contact</li>
+        <li>Full stack solution</li>
+        <li>Additional features like router from next/navigation</li>
+        <li>Optilized rendering</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
